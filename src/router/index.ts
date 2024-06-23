643200582be1354ce95b2c7f50ab42c6c7216bcb
ml <haveno@qq.com>
2024-06-23 17:23:54 +0800

    feat: nil

--- src/router/index.ts
+++ src/router/index.ts
@@ -17,6 +17,16 @@ const router = createRouter({
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue')
     }
+    ,
+    {
+      path: '{gameCode}/index',
+      name: 'index',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      // todo 换成通用的首页
+      component: () => import('../views/AboutView.vue')
+    }
   ]
 })
 
